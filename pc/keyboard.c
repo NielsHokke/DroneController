b8e92147b0103193560d48d0afdee419dfbb9d77
Nilay Sheth <n.y.sheth@student.tudelft.nl>
2018-05-06 00:57:52 +0200

    add arrow keys

--- pc/keyboard.c
+++ pc/keyboard.c
@@ -2,12 +2,16 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h> 
 #include "packet.h"
 
+
 #define OFFSET 10      // trim offest on single keypress
-#define PAYLOAD_LEN 20 // length of packet over serial
+#define PAYLOAD_LEN 30 // length of packet over serial
 #define P1 200         // velocity controller gain
 #define P2 200         // position controller gain
+
+bool terminate = false; //checks for ESC pattern
 
 int main(void)
 {
@@ -33,10 +37,10 @@ int main(void)
   tcsetattr(STDIN_FILENO, TCSANOW, &raw_term);
 
   /* keymaps
-  • ESC: abort / exit
-  • 0 mode 0, 1 mode 1, etc. (random access)
-  • a/z: lift up/down
-  • left/right arrow: roll up/down
+  • ESC: abort / exit 
+  • 0 mode 0, 1 mode 1, etc. (random access) 
+  • a/z: lift up/down 
+  • left/right arrow: roll up/down 
   • up/down arrow: pitch down/up (cf. stick)
   • q/w: yaw down/up
   • u/j: yaw control P up/down
@@ -57,34 +61,70 @@ int main(void)
       switch (ch) {
         // numbers zero to nine
         case 48 ... 57: packet.mode = ch-48; break;
+        
         case 'a'      : packet.lift = packet.lift + OFFSET; break;
-        case 'z'      : packet.lift = packet.lift - OFFSET; break;  
+        case 'z'      : packet.lift = packet.lift - OFFSET; break;
+        
+        case 'q'      : packet.yaw = packet.yaw + OFFSET; break;
+        case 'w'      : packet.yaw = packet.yaw - OFFSET; break;
+
+        /*
+        case 'u'      : packet.p1 = packet.p1 + OFFSET; break;
+        case 'j'      : packet.p1 = packet.p1 - OFFSET; break;
+		*/
+
         case 'i'      : packet.p1 = packet.p1 + OFFSET; break;
         case 'k'      : packet.p1 = packet.p1 - OFFSET; break;
+
         case 'o'      : packet.p2 = packet.p2 + OFFSET; break;
         case 'l'      : packet.p2 = packet.p2 - OFFSET; break;
-        default       : printf("unknown key 0x%02x : %c\n", ch, 
-                        (ch >= 32 && ch < 127) ? ch : ' '); 
+
+        case 27		  : read(STDIN_FILENO, &ch, 1);  // ignore character '['
+        				read(STDIN_FILENO, &ch, 1);  // up=A, dn=B, rght=C, left=D 
+        				if (ch == 'A') {//go fwd
+        					packet.pitch = packet.pitch - OFFSET;
+        				} 
+        				else if (ch == 'B') {//bckwd
+        					packet.pitch = packet.pitch + OFFSET; 
+        				}
+        				else if (ch == 'C') {//roll negative CW
+        					packet.roll = packet.roll - OFFSET;
+        				}
+        				else if (ch == 'D') {//roll positive CCW
+        					packet.roll = packet.roll + OFFSET;
+        				}
+        				else {
+        					// ESC button pressed
+        					terminate = true;
+        				}
+        				break;
+
+        default       : printf("unmapped key 0x%02x : %c\n", ch, 
+                        (ch >= 32 && ch < 127) ? ch : ' '); //terminate = true;
                         break;
       }
     }
 
-    /*-----------------------------------------
+    /*-----------------------------------------b
       compose packet 
     ------------------------------------------*/
     /*
     int snprintf(char *str, size_t size, const char *format, ...);
     */
     // remember to fix field length
-    payload_len = snprintf(payload, PAYLOAD_LEN, "%03d %03d %03d %03d", packet.mode, packet.lift, packet.p1, packet.p2);
+    payload_len = snprintf(payload, PAYLOAD_LEN, 
+    			  "%03d %03d %03d %03d %03d %03d %03d", 
+    			  packet.mode, packet.lift, 
+    			  packet.yaw, packet.pitch, packet.roll, 
+    			  packet.p1, packet.p2);
     if(payload_len <= PAYLOAD_LEN) {
-     printf("payload: %s len: %d\n", payload, payload_len);
+    	printf("payload: %s len: %d\n", payload, payload_len);
     }
     else {
-      printf("increase payload len\n");
+     	printf("increase payload len\n"); terminate = true;
     }
-    sleep(0.4);
-  } while (ch != 27);
+    usleep(100000); // Cut update rate to 10Hz
+  } while(!terminate); //while (ch != 27);
 
 
 
