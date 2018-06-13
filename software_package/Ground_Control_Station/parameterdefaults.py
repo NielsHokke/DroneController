d07493b92872ddfed516bf842fe8854348ad880c
Jetse Brouwer <Jetse.Brouwer@gmail.com>
2018-06-13 12:12:53 +0200

    updated default parameters (With factor 4,since new scaling)

--- software_package/Ground_Control_Station/parameterdefaults.py
+++ software_package/Ground_Control_Station/parameterdefaults.py
@@ -1,6 +1,6 @@
-P_Yaw = 0 #122 verified by ta as good
-P_P1 = 20 #VERFIED BY TA FOR ROLL
-P_P2 = 84 # VERIFIED BY TA FOR ROLL
+P_Yaw = 122*4 #122 verified by ta as good
+P_P1 = 20*4 #20 VERFIED BY TA FOR ROLL
+P_P2 = 84*4 #84  VERIFIED BY TA FOR ROLL
 P_yaw_min = 255
 P_yaw_max = 255
 P_angle_min = 255
